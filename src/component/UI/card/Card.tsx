5f65a9842c79c60b44bb4351e3b1e78de9c9a166
motoblock <milasasha@gmail.com>
2023-04-01 22:08:57 +0300

    sdf

--- src/component/UI/card/Card.tsx
+++ src/component/UI/card/Card.tsx
@@ -4,31 +4,26 @@ import mail from './../../../assets/images/male-logo.png';
 import woman from './../../../assets/images/woman-logo.png';
 import { ICardCatProps } from './types';
 
-export class Card extends React.Component<ICardCatProps, { value: string }> {
-  render() {
-    const link = `${this.props.image}`;
-    let sex = (
-      <img src={mail} title={this.props.description} alt={`Кот по кличке ` + this.props.name} />
-    );
-    if (this.props.sex == 1)
-      sex = (
-        <img src={woman} title={this.props.description} alt={`Кот по кличке ` + this.props.name} />
-      );
-    return (
-      <div data-testid="card" className={stCard.cards__card}>
-        <div className={stCard.card__images}>
-          <img src={link} title={this.props.description} alt={`Кот по кличке ` + this.props.name} />
-        </div>
-        <div className={stCard.card__text}>
-          <div className={stCard.card__age}>
-            {this.props.age} месяцев {sex}
-          </div>
-          <div className={stCard.card__name}>{this.props.name}</div>
-          <div className={stCard.card__breed}>{this.props.breed}</div>
-          <div className={stCard.card__cattery}>{this.props.catterys}</div>
-          <div className={stCard.card__price}>{this.props.price} ₽</div>
-        </div>
-      </div>
-    );
-  }
+function genderImg(sex: number, name: string) {
+  if (sex === 1) return <img src={mail} alt={`Кот по кличке ` + name} />;
+  return <img src={woman} alt={`Кот по кличке ` + name} />;
 }
+
+export const Card = (props: ICardCatProps) => {
+  return (
+    <div data-testid="card" className={stCard.cards__card}>
+      <div className={stCard.card__images}>
+        <img src={`${props.image}`} title={props.description} alt={`Кот по кличке ` + props.name} />
+      </div>
+      <div className={stCard.card__text}>
+        <div className={stCard.card__age}>
+          {props.age} месяцев {genderImg(props.sex, props.name)}
+        </div>
+        <div className={stCard.card__name}>{props.name}</div>
+        <div className={stCard.card__breed}>{props.breed}</div>
+        <div className={stCard.card__cattery}>{props.catterys}</div>
+        <div className={stCard.card__price}>{props.price} ₽</div>
+      </div>
+    </div>
+  );
+};
