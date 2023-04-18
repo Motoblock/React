e117c10bc5d81e7a319210235692676257a9bccf
motoblock <milasasha@gmail.com>
2023-04-19 00:00:40 +0300

    feat: add tests

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -4,7 +4,16 @@ describe('Home page test', () => {
     cy.intercept('GET', 'https://mock-server-cats.vercel.app').as('getCats');
   });
   it('Should search cards of cats ', () => {
-    cy.visit('/');
     cy.get('input[type=text]').type('брит{enter}');
+    cy.get('[data-testid="card"]').should('have.length', 20);
+  });
+
+  it('modal window works', () => {
+    cy.wait('@getCats');
+    const card = cy.get('[data-testid="card"]').first();
+    card.contains('See more...').click();
+    // cy.contains('See more...').click();
+    cy.contains('Бенедикт');
+    cy.get('[class="modal-close"]').click();
   });
 });
